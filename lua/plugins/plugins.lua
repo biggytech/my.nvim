dafb5fa176d7304595573d89316f0aab877f7185
Neli Harbuzava <ineliharbuzava@gmail.com>
2025-06-11 17:31:31 +0300

    Config Zen mode

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      styles = {
+        zen = {
+          backdrop = {
+            transparent = false,
+          },
+          width = 90,
+        },
+      },
+    },
+  },
+}
