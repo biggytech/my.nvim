461d119ddebbda04e706fc3cd4c7d226113551a2
Neli Harbuzava <ineliharbuzava@gmail.com>
2025-06-11 23:52:04 +0300

    Add custom keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+function insertFullPath()
+  local filepath = vim.fn.expand("%")
+  vim.fn.setreg("+", filepath) -- write to clippoard
+end
+
+vim.keymap.set("n", "<leader>pc", insertFullPath, { noremap = true, silent = true, desc = "Copy File Absolute Path" })
